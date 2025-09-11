53ccf40e701b0db6100c6a6343d4b81078fc1d2c
João Vitor Ibiapino <ibiapinojoaovitor@gmail.com>
2025-09-10 22:06:46 -0300

    initial project

---
+++ src/main/java/br/com/dio/model/MoneyAudit.java
@@ -0,0 +1,13 @@
+package br.com.dio.model;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public record MoneyAudit(
+        UUID trasactionId,
+        BankService targetService,
+        String description,
+        OffsetDateTime createAt)
+{
+
+}
